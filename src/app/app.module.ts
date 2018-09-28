17297af9ea1d7a9e6aa66a3eb6f25eb65f53d359
Lucca Machado <lucca.santos@acad.pucrs.br>
2018-09-28 18:18:18 -0300

    adicionada card no header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,29 @@ import { FormsModule } from '@angular/forms';
 import { appRouting } from './app-routing';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     NotFoundComponent,
-    AboutComponent 
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
     appRouting,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
