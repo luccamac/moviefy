0313c434b2c4110c0c1d3a90d3508dff5ca0c2ef
Flavio Keglevich <fkeglevich@gmail.com>
2018-09-28 14:36:06 -0300

    Implementada a pagina de sobre

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -14,7 +14,7 @@ const appRoutes: Routes = [
         path: '', 
         component: SearchComponent,
         pathMatch: 'full'
-    }
+    },
     { 
         path: '**', 
         component: NotFoundComponent
